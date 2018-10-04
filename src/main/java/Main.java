805c78500e6c2b698094bf508c8a836a20afc236
ahmetustun <ahmetustun89@gmail.com>
2018-10-04 13:54:02 +0300

    Exponential coefficient for similarity prior and switch for artifact are added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         //Designed to switch between unigram and bigram model
-        if (args[12].equalsIgnoreCase("unigram")) {
+        if (args[11].equalsIgnoreCase("unigram")) {
             unigram.Gibbs_RecursiveInference.doMain(args);
-        } else if (args[12].equalsIgnoreCase("bigram")) {
+        } else if (args[11].equalsIgnoreCase("bigram")) {
             bigram.Gibbs_RecursiveInference.doMain(args);
         }
     }
